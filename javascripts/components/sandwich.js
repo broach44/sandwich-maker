488916dfb596924da781d0794897787945cced70
Crystal Broach <crysland@gmail.com>
2019-09-25 23:39:16 -0500

    added item pricing and order total

--- javascripts/components/sandwich.js
+++ javascripts/components/sandwich.js
@@ -2,11 +2,20 @@ import utilities from "../helpers/utilities.js";
 import bread from "./bread.js";
 
 const createFinalOrder = (items) => {
-    let domString2 = `<p>Thank you for your order!</p>`;
+    let domString2 = `<div><br><p>Thank you for your order!</p><br>`;
+    let sum = 0;
     for (let i=0; i < items.length; i++) {
+        let priceNumber = items[i].price
+        sum += priceNumber;
+        priceNumber /= 100;
+        let dollars = priceNumber.toLocaleString("en-US", {style:"currency", currency:"USD"});
         domString2 += `
-        <p>${items[i].name} ${items[i].price}</p>`
+        <p>${items[i].name} ${dollars}</p>
+        `
     };
+    sum /= 100;
+    const totalDollars = sum.toLocaleString("en-US", {style:"currency", currency: "USD"});
+    domString2 += `<hr><p>Your total is: ${totalDollars}</p></div>`;
     utilities.printToDom('final-order', domString2);
 };
 
